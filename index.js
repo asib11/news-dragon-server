3a33196f8dfbf99b7cfbcaf6923940320ad52ecc
asib ahmed <ahmedasib1@gmail.com>
2023-11-15 22:47:44 +0600

    load json ddata

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const port = process.env.PORT || 5000;
 
+const catagories = require('./catagories.json');
+
+app.use(cors());
 app.get('/', (req, res) =>{
     res.send('dragon is running');
 });
+
+app.get('/catagories', (req, res) =>{
+    res.send(catagories);
+})
 
 app.listen(port, (req, res) =>{
     console.log(`server running: ${port}`)
